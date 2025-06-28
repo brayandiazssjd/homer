687946e3086a9dafdcdaf4b4a3ab39146d988d9b
brayandiazssjd <brayandiazssjd@gmail.com>
2025-06-27 22:55:25 -0500

    Added Connection class.

--- src/main/java/com/project/homer/model/DBConnection.java
+++ src/main/java/com/project/homer/model/DBConnection.java
@@ -1,7 +1,31 @@
 package com.project.homer.model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DBConnection {
-    
+public final class DBConnection {
+    private static String url = System.getenv("DATABASE_URL");
+    private static String user = System.getenv("DATABASE_USER");
+    private static String password = System.getenv("DATABASE_PASSWORD");
+    private static Connection instance = null;
+
+    private DBConnection() {
+        try {
+            if (instance == null) {
+                instance = DriverManager.getConnection(url, user, password);
+            }
+        } catch (SQLException sqle) {
+            System.out.println(sqle.toString());
+        }
+    }
+
+    public static Connection getInstance() {
+        DBConnection(); 
+        return instance;
+    }
+
+    public void disconnect() {
+        instance = null;
+    }
 }
