25d7fb0f0b3d1c05805e3017db4409b121034bbc
brayandiazssjd <brayandiazssjd@gmail.com>
2025-06-29 16:21:53 -0500

    Added update and select example for Service

--- src/main/java/com/project/homer/controller/DAOs/ServiceDAO.java
+++ src/main/java/com/project/homer/controller/DAOs/ServiceDAO.java
@@ -1,9 +1,47 @@
 package com.project.homer.controller.DAOs;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Statement;
+
+import com.project.homer.model.DBConnection;
+import com.project.homer.model.Service;
+
 /**
  * ServiceDAO
  */
 
 public class ServiceDAO {
-    
+    ArrayList<Service> cached;
+
+    public ArrayList<Service> getFullAll() throws SQLException {
+        String query = "select * from servicio";
+        Connection c = DBConnection.getInstance();
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        
+        ArrayList<Service> l = new ArrayList<>();
+
+        while (rs.next()) {
+            Service s = new Service();
+            s.setName(rs.getString("nombre"));
+            s.setId(rs.getInt("idservicio"));
+            s.setDescription(rs.getString("serdescripcion"));
+            l.add(s);
+        }
+        return l;
+    }
+
+    public void fullUpdate(Service s) throws SQLException {
+        String query = "update servicio set descripcion = ?, id = ?, nombre = ? where id = ?";
+        Connection c = DBConnection.getInstance();
+        PreparedStatement ps = c.prepareStatement(query);
+        ps.setString(s.getDescription());
+        ps.setInt(s.getId());
+        ps.setString(s.getName());
+        ps.executeUpdate();
+    }
 }
